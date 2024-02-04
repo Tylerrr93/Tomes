dca27cec36219011803195dbe31b91c3d80d841e
Tylerrr93 <tylerrr93@gmail.com>
2024-02-04 13:42:33 -0500

    --typecast crash solved
    -capi null reference error fixed
    (methods run twice client and server side?)

--- src/BlockEntityGutenbergPress.cs
+++ src/BlockEntityGutenbergPress.cs
@@ -79,17 +79,17 @@ namespace Tomes
             ItemSlot handslot = byPlayer.InventoryManager.ActiveHotbarSlot;
             ItemStack handStack = handslot.Itemstack;
 
-            // Check if the player's hand slot is empty and the bucket slot is not empty
+            // Check if the player's hand slot is empty and the frisket slot is not empty
             if (handslot.Empty && !FrisketSlot0.Empty)
             {
-                // Give the itemstack in the bucket slot to the player
+                // Give the itemstack in the frisket slot to the player
                 if (!byPlayer.InventoryManager.TryGiveItemstack(FrisketSlot0.Itemstack, true))
                 {
                     // If giving fails, spawn the item entity in the world
                     Api.World.SpawnItemEntity(FrisketSlot0.Itemstack, Pos.ToVec3d().Add(0.5, 0.5, 0.5));
                 }
 
-                // Play the place sound of the item in the bucket slot
+                // Play the place sound of the item in the frisket slot
                 if (FrisketSlot0.Itemstack.Block != null)
                     //Api.World.PlaySoundAt(FrisketSlot0.Itemstack.Block.Sounds.Place, Pos.X + 0.5, Pos.Y, Pos.Z + 0.5, byPlayer);
                     //Currently causes null errors, fix this to play sounds (handstack field)
@@ -99,10 +99,10 @@ namespace Tomes
                 MarkDirty(true);
                 meshTypecast?.Clear();
             }
-            // Check if the player's hand slot contains an item and it's a liquid container block
+            // Check if the player's hand slot contains an item and it's a parchment
             else if (handStack != null && handStack.Collectible.Code.Path == "paper-parchment" && FrisketSlot0.Empty)
             {
-                // Try putting the item from the player's hand slot into the bucket slot
+                // Try putting the item from the player's hand slot into the frisket slot
                 bool moved = handslot.TryPutInto(Api.World, FrisketSlot0, 1) > 0;
 
                 // If the item is successfully moved, update slots, mesh, and play the place sound
@@ -121,10 +121,13 @@ namespace Tomes
 
         private void generateMeshTypecast() {
 
+        // Checks to make sure capi is not null, will crash if it is. Best guess is this runs server and client side so we need this to prevent crashes
+        // if capi is null(?). Not too sure - but definitely need this line. 
+        if (FrisketSlot0.Empty || capi == null) return;
+
         // Load the typecast mesh and set it as meshSource
         Shape shapeTypecast = Shape.TryGet(Api, "tomes:shapes/gutenbergpress/part-typecast.json");
         var meshSource = shapeTypecast;
-
 
         // If loaded sucessfully...
         if (meshSource != null) {
